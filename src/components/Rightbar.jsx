c0a7a00096eb1a2780d09f124851652557b643de
long <longvnbd2000@gmail.com>
2021-11-22 01:11:03 +0700

    right bar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -3,7 +3,143 @@
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            right
+            <div className="rightbar-title">User Online</div>
+            <ul className="rightbar-online-list">
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+                <li className="rightbar-online-list-items">
+                    <div className="rightbar-online-list-item-avatar">
+                        <img src="assets/avatar/ok.jpg" alt="" />
+                        <span className="rightbar-online-tick"></span>
+                    </div>
+                    <span className="rightbar-online-list-item-user">Ducky</span>
+                </li>
+            </ul>
         </div>
     )
 }
