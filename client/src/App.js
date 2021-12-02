d4e2c15996e9336be8256c786670b14c4afb58be
long <longvnbd2000@gmail.com>
2021-12-02 11:49:46 +0700

    client folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Announcement from "./pages/Announcement";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SigninPage from "./pages/SigninPage";
+
+function App() {
+  return (
+    <SigninPage/>
+  );
+}
+
+export default App;
---
+++ client/src/components/Signin/Signin.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import {GoogleLogin} from 'react-google-login'
+import './Signin.css'
+
+export default function Signin() {
+
+    const [toggle, setToggle] = useState('toggle')
+    const [toggleBtnStd, setToggleBtnStd] = useState('toggle-btn std or')
+    const [toggleBtnOth, setToggleBtnOth] = useState('toggle-btn oth')
+    const [students, setStudents] = useState('students')
+    const [others, setOthers] = useState('others')
+
+    const studentSignin = () => {
+        setToggle('toggle')
+        setStudents('students') 
+        setOthers('others')
+        setToggleBtnOth('toggle-btn oth')
+        setToggleBtnStd('toggle-btn std or')
+    }
+    const othersSignin = () => {
+        setToggle('toggle or')
+        setStudents('students or') 
+        setOthers('others or')
+        setToggleBtnOth('toggle-btn oth or')
+        setToggleBtnStd('toggle-btn std')
+    }
+
+    return (
+        <>
+        <div className="signin-container">
+            
+            <div className="signin-form">
+                <h1 className="logo">TDTU</h1>
+                <div className="toggle-box">
+                    <div className={toggle}></div>
+                    <button type="button" className={toggleBtnStd} onClick={studentSignin}>Students</button>
+                    <button type="button" className={toggleBtnOth} onClick={othersSignin}>Others</button>
+                </div>
+
+
+                <form action="" className={others}>
+                    <div className="form-group">
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <br/>
+                    <div className="form-group">  
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                    <button className="submit-btn" type="submit">Sign in</button>
+                </form>
+
+                <div className={students}>
+                    <div>@student.tdtu.edu.vn</div>
+
+                    <GoogleLogin
+                        className="signin-gg-btn"
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                    
+                        
+                        cookiePolicy={'single_host_origin'}
+                    />
+                    
+                </div>
+                
+            </div>
+            
+        </div>  
+        </>
+    )
+}
